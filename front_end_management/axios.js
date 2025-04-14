8cac2826f9355504623c49d2f1fc5e932a24279c
NguyenTK <tkhoinguyen117@gmail.com>
2025-04-14 16:49:21 +0700

    config for vercel

--- front_end_management/axios.js
+++ front_end_management/axios.js
@@ -1,6 +1,6 @@
     import axios from "axios";
 const apiClient = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://e04c-2405-4800-1f06-3200-3496-9264-a4c0-93f.ngrok-free.app',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
